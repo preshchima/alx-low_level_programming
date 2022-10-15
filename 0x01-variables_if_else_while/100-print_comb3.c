ecd59daef43ab78cb3baeaf3d12a372202222f87
preshchima <preshchima@gmail.com>
2022-10-14 19:38:21 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,7 @@ int main(void)
 
 	for (d1 = 0; d1 < 9; d1++)
 	{
-		for (d2 = d1 + 1; d2 < 10; d2)
+		for (d2 = d1 + 1; d2 < 10; d2++)
 		{
 			putchar((d1 % 10) + '0');
 			putchar((d2 % 10) + '0');
