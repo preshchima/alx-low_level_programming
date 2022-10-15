6627410a7f59ce9c2fad6fbfbeb7e078cf775bf1
preshchima <preshchima@gmail.com>
2022-10-14 18:54:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Description: prints the numbers in base 16
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char lw;
+
+	for (num = 0; num < 10; numm++)
+		putchar((num % 10) + '0');
+
+	for (lw = 'a'; lw <= 'f'; lw++)
+		putchar(lw);
+
+	putchar('\n');
+
+	return (0);
+}
