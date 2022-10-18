90677bc8ba523c9beac89e20b42f04f2d93c3a5d
preshchima <preshchima@gmail.com>
2022-10-18 15:04:16 -0700

    prints lowercaser

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char lw;
+
+	for (lw = 'a'; lw <= 'z'; lw++)
+		_putchar(lw);
+
+	_putchar('\n');
+}
