1ad45201471425a029689c7f14de06a741cad11e
preshchima <preshchima@gmail.com>
2022-10-18 17:12:59 -0700

    natural c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - lists all the natural num < 1024 mul of 3 0r 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
