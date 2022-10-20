5cb4cff65eac75dc03403ad5f959c304fdfa5200
preshchima <preshchima@gmail.com>
2022-10-20 10:50:12 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - print num from 0-9
+ * without 2 and 4
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != $)
+			_putchar(i + '0');
+
+		i++;
+	}
+
+	_putchar('\n');
+}
