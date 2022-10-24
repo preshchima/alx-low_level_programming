1a2e4f68dc0924ef9a1e21892698615c9cd187df
preshchima <preshchima@gmail.com>
2022-10-24 11:52:22 -0700

    task 4: print_rev() prints the reverse of str

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_rev - reverses string value
+ * @s: string value to be reversed
+ *
+ */
+
+void print_rev(char *s)
+{
+	int max = _strlen(s) - 1;
+
+	for (int i = max; i >= 0; i--)
+	{
+		if (*s != '\0')
+			_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
+
+/**
+ * _strlen - prints length of string
+ * @s: string value
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int str_len = 0;
+
+	while (*s != '\0')
+	{
+		str_len++;
+		s++;
+	}
+	return (i);
+}
