f6fa78a59299fabb39379dbc063eb6dfe0cee32d
preshchima <preshchima@gmail.com>
2022-10-24 21:36:45 -0700

    task 6: puts2(): prints every other str char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts2 - prints a str
+ * @str: string value
+ *
+ * Description: prints every other character of a string, starting with the
+ * first character, followed by a new line
+ */
+
+void _puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
