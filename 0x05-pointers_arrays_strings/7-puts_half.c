7ac6112828e87d2c09730db32d436a64e2f32cd7
preshchima <preshchima@gmail.com>
2022-10-24 21:51:01 -0700

    task 7: puts_half(): prints out the first half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prints out first half of string
+ * @str: string value
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i + 1) / 2;
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
