887333b9b337cb65f832cd6dff6021601aad8797
preshchima <preshchima@gmail.com>
2022-10-24 21:15:01 -0700

    task 5: rev_string(): reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ * @s: string to be reversed
+ *
+ */
+
+void rev_string(char *s)
+{
+	int max, i;
+
+	char tmp;
+
+	max = _strlen(s) - 1;
+
+	for (i = 0, i < max; i++)
+	{
+		tmp = s[i];
+		*(s + i) = s[max];
+		s[max] = tmp;
+		max--;
+	}
+}
+
+/**
+ * _strlen - gets string length
+ * @s: string value
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
