b09a5606993b81da7005b93d42142c777c7fddfd
preshchima <preshchima@gmail.com>
2022-10-25 20:33:18 -0700

    task 11; generate rand valid password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random valid password
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int p[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		p[i] = rand() % 78;
+		sum += (p[i] + '0');
+		putchar(p[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
