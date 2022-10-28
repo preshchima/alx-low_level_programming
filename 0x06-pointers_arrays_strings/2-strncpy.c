6fa575e47635df61b4235805a4779ee7ca1fd3a3
preshchima <preshchima@gmail.com>
2022-10-27 17:36:47 -0700

    task 2: [UPDATE]

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	int i;
 
-	for (i = 0; i < n; i++)
-	{
-		if (*src != '\0')
-		{
-			*(dest + i) = src[i];
-		}
-	}
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+	while (i < n)
+		dest[i++] = '\0';
+
 	return (dest);
 }
