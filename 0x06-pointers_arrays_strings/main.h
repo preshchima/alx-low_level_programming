bf28d65ed8d56402bead28f33246ceeddbeacd1c
preshchima <preshchima@gmail.com>
2022-10-28 19:51:05 -0700

    task 104

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *cap_string(char *);
 int _strlen(char *str);
 char *rot13(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
 
